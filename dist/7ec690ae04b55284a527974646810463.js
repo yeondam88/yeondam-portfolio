a76d7f3eb6e8a91ebf967ca0a497f39cb29fb8e8
Yeondam Park <giyen9088@gmail.com>
2018-09-08 22:08:17 -0700

    upload to github

---
+++ dist/7ec690ae04b55284a527974646810463.js
@@ -0,0 +1,352 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+require = (function (modules, cache, entry) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof require === "function" && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof require === "function" && require;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({8:[function(require,module,exports) {
+var bundleURL = null;
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+
+  return bundleURL;
+}
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp):\/\/[^)\n]+/g);
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp):\/\/.+)\/[^/]+$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],5:[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+  newLink.onload = function () {
+    link.remove();
+  };
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":8}],4:[function(require,module,exports) {
+
+        var reloadCSS = require('_css_loader');
+        module.hot.dispose(reloadCSS);
+        module.hot.accept(reloadCSS);
+      
+},{"./../images/bg.svg":[["3a4ab4878e2c53189bf37e908ae24265.svg",6],6],"./../images/bg_2.svg":[["5f71d12645d0ae91e1fb49f4f3d29074.svg",7],7],"_css_loader":5}],3:[function(require,module,exports) {
+"use strict";
+
+require("../scss/main.scss");
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+(function () {
+  var exit = document.querySelector(".close");
+  var portfolio = document.querySelector(".portfolio__container");
+  var portfolioDetail = document.querySelector(".portfolio__detail");
+
+  portfolio.addEventListener("click", function () {
+    console.log("clicked!");
+    portfolioDetail.classList.add("open");
+  });
+
+  exit.addEventListener("click", function () {
+    portfolioDetail.classList.remove("open");
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    "use strict";
+
+    var root = function () {
+      if ("scrollingElement" in document) return document.scrollingElement;
+      var html = document.documentElement;
+      var start = html.scrollTop;
+      html.scrollTop = start + 1;
+      var end = html.scrollTop;
+      html.scrollTop = start;
+      return end > start ? html : document.body;
+    }();
+
+    var ease = function ease(duration, elapsed, start, end) {
+      return Math.round(end * (-Math.pow(2, -10 * elapsed / duration) + 1) + start);
+    };
+
+    var getCoordinates = function getCoordinates(hash) {
+      var start = root.scrollTop;
+      var delta = function () {
+        if (hash.length < 2) return -start;
+        var target = document.querySelector(hash);
+        if (!target) return;
+        var top = target.getBoundingClientRect().top;
+        var max = root.scrollHeight - window.innerHeight;
+        return start + top < max ? top : max - start;
+      }();
+      if (delta) return new Map([["start", start], ["delta", delta]]);
+    };
+
+    var scroll = function scroll(link) {
+      var hash = link.getAttribute("href");
+      var coordinates = getCoordinates(hash);
+      if (!coordinates) return;
+
+      var tick = function tick(timestamp) {
+        progress.set("elapsed", timestamp - start);
+        root.scrollTop = ease.apply(undefined, _toConsumableArray(progress.values()).concat(_toConsumableArray(coordinates.values())));
+        progress.get("elapsed") < progress.get("duration") ? requestAnimationFrame(tick) : complete(hash, coordinates);
+      };
+
+      var progress = new Map([["duration", 800]]);
+      var start = performance.now();
+      requestAnimationFrame(tick);
+    };
+
+    var complete = function complete(hash, coordinates) {
+      history.pushState(null, null, hash);
+      root.scrollTop = coordinates.get("start") + coordinates.get("delta");
+    };
+
+    var attachHandler = function attachHandler(links, index) {
+      var link = links.item(index);
+      link.addEventListener("click", function (event) {
+        event.preventDefault();
+        scroll(link);
+      });
+      if (index) return attachHandler(links, index - 1);
+    };
+
+    var links = document.querySelectorAll("a.scroll");
+    var last = links.length - 1;
+    if (last < 0) return;
+    attachHandler(links, last);
+  });
+})();
+},{"../scss/main.scss":4}],43:[function(require,module,exports) {
+
+var global = (1, eval)('this');
+var OldModule = module.bundle.Module;
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    accept: function (fn) {
+      this._acceptCallback = fn || function () {};
+    },
+    dispose: function (fn) {
+      this._disposeCallback = fn;
+    }
+  };
+}
+
+module.bundle.Module = Module;
+
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = '' || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '53180' + '/');
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      data.assets.forEach(function (asset) {
+        hmrApply(global.require, asset);
+      });
+
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          hmrAccept(global.require, asset.id);
+        }
+      });
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + 'data.error.stack');
+    }
+  };
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(+k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  var cached = bundle.cache[id];
+  if (cached && cached.hot._disposeCallback) {
+    cached.hot._disposeCallback();
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallback) {
+    cached.hot._acceptCallback();
+    return true;
+  }
+
+  return getParents(global.require, id).some(function (id) {
+    return hmrAccept(global.require, id);
+  });
+}
+},{}]},{},[43,3])
+//# sourceMappingURL=/dist/7ec690ae04b55284a527974646810463.map
